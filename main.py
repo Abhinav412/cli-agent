2c0613fd21798f5d5835206217515d7f9a0a95cc
Abhinav <abhinavdasu0412@gmail.com>
2025-07-05 22:33:47 +0530

    calculator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 def main():
     if len(sys.argv) < 2:
@@ -11,14 +12,19 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     
+    messages = [types.Content(role="user",parts=[types.Part(text=prompt)]),]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model = "gemini-2.0-flash-001", 
-    contents= prompt)
+    contents= messages)
     
     print(response.text)
-    print("Prompt tokens:",response.usage_metadata.prompt_token_count)
-    print("Response tokens:",response.usage_metadata.candidates_token_count)
 
+    if sys.argv[2] == "--verbose":
+        print("User prompt:",prompt)
+        print("Prompt tokens:",response.usage_metadata.prompt_token_count)
+        print("Response tokens:",response.usage_metadata.candidates_token_count)
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
